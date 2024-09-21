8a184be438dd37eac1191e0eeb58357db9b511a4
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-22 00:31:46 +0600

    javascript code updated

--- js/index.js
+++ js/index.js
@@ -2,9 +2,25 @@ let seatNumberElement = document.getElementById("seat-number-element");
 let seatDecreaseElement = document.getElementById("seat-decrease");
 let noBookedHeading = document.getElementById("no-booked");
 let parentUL = document.getElementById("parentUl");
+let ticketPrice = 0;
+let totalPriceBDT = document.getElementById("total-price-bdt");
+let couponInput = document.getElementById("coupon-input");
+let couponApplyBtn = document.getElementById("coupon-apply-button");
+let discountDivElement = document.getElementById("discount-div");
+let discountPriceElement = document.getElementById("discount-price");
+let grandTotalPrice = document.getElementById("grand-total");
+let passengerName = document.getElementById("Passenger-name");
+let passengerPhone = document.getElementById("Passenger-phone");
+let passengerEmail = document.getElementById("Passenger-email");
+let formBtn = document.getElementById("form-btn");
 let arr = [];
 function ticketBooked(event) {
   let buttonClicked = event.target;
+
+  if (arr.includes(buttonClicked.innerText)) {
+    alert("This seat already booked");
+    return;
+  }
 
   buttonClicked.classList.add("bg-[#1DD100]");
   buttonClicked.style.color = "white";
@@ -26,5 +42,53 @@ function ticketBooked(event) {
                 <li>550</li>
               </div>
   `;
-  console.log(parentUL);
+
+  ticketPrice = ticketPrice + 550;
+  totalPriceBDT.innerText = ticketPrice;
+
+  if (arr.length > 3) {
+    couponInput.removeAttribute("disabled");
+    couponApplyBtn.removeAttribute("disabled");
+  }
 }
+
+couponApplyBtn.addEventListener("click", function () {
+  if (couponInput.value !== "NEW15" && couponInput.value !== "Couple 20") {
+    alert("wrong coupon provided");
+    return;
+  }
+
+  if (couponInput.value === "NEW15") {
+    let discount15 = ticketPrice * 0.15;
+    ticketPrice = ticketPrice - discount15;
+    discountPriceElement.innerText = discount15;
+    couponInput.classList.add("hidden");
+    couponApplyBtn.classList.add("hidden");
+    discountDivElement.classList.remove("hidden");
+    discountDivElement.classList.add("flex");
+    grandTotalPrice.innerText = ticketPrice;
+    passengerName.removeAttribute("disabled");
+    passengerPhone.removeAttribute("disabled");
+    passengerEmail.removeAttribute("disabled");
+  }
+
+  if (couponInput.value === "Couple 20") {
+    let discount20 = ticketPrice * 0.2;
+    ticketPrice = ticketPrice - discount20;
+    discountPriceElement.innerText = discount20;
+    couponInput.classList.add("hidden");
+    couponApplyBtn.classList.add("hidden");
+    discountDivElement.classList.remove("hidden");
+    discountDivElement.classList.add("flex");
+    grandTotalPrice.innerText = ticketPrice;
+    passengerName.removeAttribute("disabled");
+    passengerPhone.removeAttribute("disabled");
+    passengerEmail.removeAttribute("disabled");
+  }
+});
+
+passengerPhone.addEventListener("input", function () {
+  if (passengerPhone.value.length > 10) {
+    formBtn.removeAttribute("disabled");
+  }
+});
